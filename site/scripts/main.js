0f25b5c3f62a7c8602e3f58d4589e037d6ace373
talrez11 <tal@way2cu.com>
2015-02-22 11:54:29 +0200

    Added changes to main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -49,6 +49,44 @@ Site.is_mobile = function() {
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
+
+	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
+
+	if ($('div.gallery_container a').length > 0) {
+		clientGallery = new Caracal.Gallery.Slider();
+		clientGallery
+		.images.set_container('div.gallery_container')
+		.images.add('div.gallery_container a.image')
+		.controls.set_auto(3000)
+		clientGallery.images.set_center(true)
+		clientGallery.images.set_spacing(20)
+		clientGallery.images.set_visible_count(5)
+		.controls.attach_next($('div.clients_wrap a.arrow.next'))
+		.controls.attach_previous($('div.clients_wrap a.arrow.previous'));
+	};
+
+	/*
+	*** script for showing each gallery in the gallery page
+	*/
+
+	$("ul.galleries_names li").eq(0).addClass("active");
+		gallery_type = new PageControl('div.galleries_images', 'div.all_gallery')
+		gallery_type.attachControls('ul.galleries_names li');
+
+	if ($('div.galleries_images a').length > 0) {
+		galleryPortfolio = new Caracal.Gallery.Slider();
+		galleryPortfolio
+		.images.set_container('div.galleries_images')
+		.images.add('div.galleries_images a.image')
+		.controls.set_auto(3000)
+		galleryPortfolio.images.set_step_size(3)
+		galleryPortfolio.images.set_center(true)
+		galleryPortfolio.images.set_spacing(20)
+		galleryPortfolio.images.set_visible_count(3)
+		galleryPortfolio.images.set_visible_count(3)
+		.controls.attach_next($('div.gallery_control a.arrow.next'))
+		.controls.attach_previous($('div.gallery_control a.arrow.previous'));
+	};
 };
 
 
