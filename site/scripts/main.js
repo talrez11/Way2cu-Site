d77a3e1d6bef0ca4eefdb0ae94d6a06cca289d53
talrez11 <tal@way2cu.com>
2015-02-25 09:22:14 +0200

    Added changes to main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -51,7 +51,43 @@ Site.is_mobile = function() {
 Site.on_load = function() {
 
 	Caracal.lightbox = new LightBox('a.image.direct', false, false, true);
+    //Clients Gallery
+	if ($('div.gallery_container a').length > 0) {
+		clientGallery = new Caracal.Gallery.Slider();
+		clientGallery
+				.controls.attach_next($('div#clients.wrap a.arrow.next'))
+                .controls.attach_previous($('div#clients.wrap a.arrow.previous'))
+                .controls.set_auto(3000)
+                .images.set_container('div.gallery_container')
+                .images.add('div.gallery_container a.image')
+                .images.set_visible_count(5)
+                .images.set_center(true)
+                .images.set_spacing(20);
 
+	}
+	//Portfolio Gallery
+	if ($('div.all_gallery a').length > 0) {
+		galleryPortfolio = new Caracal.Gallery.Slider();
+		galleryPortfolio
+				.controls.attach_next($('div.gallery_control a.arrow.next'))
+				.controls.attach_previous($('div.gallery_control a.arrow.previous'))
+				.controls.set_auto(3000)
+				.images.set_container('div.all_gallery')
+				.images.add('div.all_gallery a.image')
+				.images.set_step_size(3)
+				.images.set_center(true)
+				.images.set_spacing(20)
+				.images.set_visible_count(3);
+	}
+	/*
+	*** script for showing each gallery in Portfolio Gallery
+	*/
+	$('ul.galleries_names li').on('click',function(){
+		var item = $(this);
+		var gallery_id = item.data('gallery');
+		galleryPortfolio.load_from_group(null, gallery_id);
+	});
+	// Function that resets position after scroll Animation
 	function resetPosition(){
 		 $('div.wrap.whitebg div.inner_wrap > span').removeClass('animation');
 		 $('div.wrap.whitebg div.inner_wrap > span').css('background-position','0px 0px');
@@ -60,8 +96,9 @@ Site.on_load = function() {
 	     $('header h1,header p').css('display','block');
 		 $('header a').css('display','inline-block');
 		 $('header div.inner_wrap > a:nth-of-type(3)').css('display','block');
-
 	}
+
+
 
 	//Scroll Function
 	$('a[href*=#]').bind('click', function(e) {
@@ -70,10 +107,10 @@ Site.on_load = function() {
 	var target = $(this).attr("href"); //Get the target
 
 	if(target == "#about") {
-
 		$('div.wrap.whitebg div.inner_wrap > span').css('background-position', '0px -250px');
 		$('div.wrap.whitebg div.inner_wrap > span').addClass('animation');
-		$('header span').css({'position':'fixed',
+		$('header span')
+						.css({'position':'fixed',
 							   'z-index':'1',
 							   'top':'82px',
 							   'left':'50%',
@@ -82,7 +119,6 @@ Site.on_load = function() {
 		$('header h1,header p').css('display','none');
 		$('header a').css('display','none');
 		$('html, body').stop().animate({ scrollTop: $(target).offset().top -82 }, 1500, function() {
-
 		  setTimeout(resetPosition,500);
 	     // location.hash = target;  //attach the hash (#jumptarget) to the pageurl
 		});
@@ -94,64 +130,19 @@ Site.on_load = function() {
 	if(target == "#clients") {
 		$('div.wrap div.inner_wrap h2').css('opacity','0');
 		$('html, body').stop().animate({ scrollTop: $(target).offset().top  }, 800, function() {
-	     location.hash = target;  //attach the hash (#jumptarget) to the pageurl
 	     $('div.wrap div.inner_wrap h2').css('opacity','1');
 		});
 
 		return false;
-
 	}
-
-
-
 	// perform animated scrolling by getting top-position of target-element and set it as scroll target
 	$('html, body').stop().animate({ scrollTop: $(target).offset().top  }, 800, function() {
 	     location.hash = target;  //attach the hash (#jumptarget) to the pageurl
-	});
+		});
 
-	return false;
+		return false;
    });
-
-	if ($('div.gallery_container a').length > 0) {
-		clientGallery = new Caracal.Gallery.Slider();
-		clientGallery
-		.images.set_container('div.gallery_container')
-		.images.add('div.gallery_container a.image')
-		.controls.set_auto(3000)
-		clientGallery.images.set_center(true)
-		clientGallery.images.set_step_size(1)
-		clientGallery.images.set_spacing(20)
-		clientGallery.images.set_visible_count(5)
-		.controls.attach_next($('div#clients.wrap a.arrow.next'))
-		.controls.attach_previous($('div#clients.wrap a.arrow.previous'));
-	};
-
-
-
-	if ($('div.all_gallery a').length > 0) {
-		galleryPortfolio = new Caracal.Gallery.Slider();
-		galleryPortfolio
-		.images.set_container('div.all_gallery')
-		.images.add('div.all_gallery a.image')
-		.controls.set_auto(3000)
-		galleryPortfolio.images.set_step_size(3)
-		galleryPortfolio.images.set_center(true)
-		galleryPortfolio.images.set_spacing(20)
-		galleryPortfolio.images.set_visible_count(3)
-		.controls.attach_next($('div.gallery_control a.arrow.next'))
-		.controls.attach_previous($('div.gallery_control a.arrow.previous'));
-	};
-
-	/*
-	*** script for showing each gallery in the gallery page
-	*/
-	$('ul.galleries_names li').on('click',function(){
-		var item = $(this);
-		var gallery_id = item.data('data-gallery');
-		galleryPortfolio.load_from_group(null, gallery_id);
-	});
-
-};
+}
 
 
 // connect document `load` event with handler function
